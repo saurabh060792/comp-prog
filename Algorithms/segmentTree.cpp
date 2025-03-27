0439065ef2d13261ce942f3250bc02ad1c20af5e
Saurabh Hota <saurabh060792@gmail.com>
2025-03-26 19:00:55 -0500

    Update segmentTree.cpp

--- Algorithms/segmentTree.cpp
+++ Algorithms/segmentTree.cpp
@@ -1,80 +1,90 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
+#include <iostream>
 
-#define MAXARRAY 100000
-#define MAXTREE 262150
+#define MAXARRAY 1000010
+#define MAXTREE 4000010
+
+using namespace std;
 
 typedef long long int lli;
 
-void buildST(lli *A, lli *segmentTree, lli node, lli low, lli high);
-lli queryST(lli *segmentTree, lli node, lli low, lli high, lli a, lli b);
-void updateST(lli *segmentTree, lli node, lli low, lli high, lli position, lli newValue);
+struct segment_tree_node {
+    lli zeros = 0;
+};
 
+segment_tree_node combine(segment_tree_node left_node, segment_tree_node right_node);
+void buildST(int node, int tl, int tr);
+segment_tree_node queryST(int node, int tl, int tr, int l, int r);
+void updateST(int node, int tl, int tr, int position, segment_tree_node value);
 
-int main()
-{
-	lli *A,i,n,*segmentTree,a,b,q,result;
-	char queryType[10];
+int n;
+segment_tree_node tree[MAXTREE], arr[MAXARRAY];
 
-	A = (lli*)malloc(MAXARRAY*sizeof(lli));
-	segmentTree = (lli*)malloc(MAXTREE*sizeof(lli));
+int main() {
+    int m, l, r, temp, query_type, k, p, x;
+    cin>>n>>m;
+    for (int i = 0; i < n; i++) {
+        cin>>temp;
+        if (temp == 0) arr[i].zeros = 1;
+    }
+ 
+    buildST(1, 0, n - 1);
 
-	scanf("%lld",&n);
-	for(i=0;i<n;i++) scanf("%lld",&A[i]);
-	scanf("%lld",&q);
-	buildST(A, segmentTree, 0, 0, n-1);
-	
-	while(q--) {
-		scanf("%s",queryType);
-		if(!strcmp(queryType,"change")) {
-			scanf("%lld%lld",&a,&b);
-			a--;
-			updateST(segmentTree,0,0,n-1,a,b);
-		}
-		else if(!strcmp(queryType,"query")) {
-			scanf("%lld%lld",&a,&b);
-			a--;b--;
-			result = queryST(segmentTree,0,0,n-1,a,b);
-		}
-	}
-
-
-	return 0;
+    for (int i = 0; i < m; i++) {
+        cin>>query_type;
+        if (query_type == 1) {
+            cin>>k;
+            int kthZeroIndex = findKthZero(1, 0, n - 1, k);
+            if (kthZeroIndex == -1) cout<<"NO"<<endl;
+            else cout<<kthZeroIndex<<endl;
+        } else {
+            cin>>p>>x;
+            segment_tree_node x_node = {};
+            x_node.zeros = x == 0 ? 1 : 0;
+            updateST(1, 0, n - 1, p, x_node);
+        }
+    }
+ 
+    return 0;
 }
 
-void buildST(lli *A, lli *segmentTree, lli node, lli low, lli high)
-{
-	lli mid = (low+high)/2,lNode = 2*node + 1,rNode = 2*node + 2;
-	if(low == high) {
-		segmentTree[node] = A[low];
-		return;
-	}
-	buildST(A,segmentTree,lNode,low,mid);
-	buildST(A,segmentTree,rNode,mid+1,high);
-	segmentTree[node] = segmentTree[lNode] + segmentTree[rNode];
-	return;
+segment_tree_node combine(segment_tree_node left_node, segment_tree_node right_node) {
+    segment_tree_node res;
+    res.zeros = left_node.zeros + right_node.zeros;
+    return res;
 }
 
-lli queryST(lli *segmentTree, lli node, lli low, lli high, lli a, lli b)
-{
-	lli mid = (low+high)/2,lNode = 2*node + 1,rNode = 2*node + 2,r1,r2;
-	if(a>high || b<low) return 0;
-	if(a<=low && high<=b) return segmentTree[node];
-
-	r1 = queryST(segmentTree, lNode, low, mid, a, b);
-	r2 = queryST(segmentTree, rNode, mid + 1, high, a, b);
-	return r1 + r2;
+// Call with node = 1, tl = 0, tr = n - 1
+void buildST(int node, int tl, int tr) {
+    if(tl == tr) {
+        tree[node] = arr[tl];
+        return;
+    }
+    int tm = (tl + tr)>>1, left_node = node<<1, right_node = node<<1|1;
+    buildST(left_node, tl, tm); // left node will be 2*node
+    buildST(right_node, tm + 1, tr);  // right node will be 2*node+1
+    tree[node] = combine(tree[left_node], tree[right_node]);
+    return;
 }
 
-void updateST(lli *segmentTree, lli node, lli low, lli high, lli position, lli newValue)
-{
-	lli mid = (low+high)/2,lNode = 2*node + 1,rNode = 2*node + 2;
-	if(low == high) {
-		segmentTree[node] = newValue;
-		return;
-	}
-	else if(position <= mid) updateST(segmentTree, lNode, low, mid, position, newValue);
-	else updateST(segmentTree, rNode, mid+1, high, position, newValue);
-	segmentTree[node] = segmentTree[lNode] + segmentTree[rNode];
+// Call with node = 1 as segment tree array starts with 1.
+// Query in interval [l, r] inclusive.
+segment_tree_node queryST(int node, int tl, int tr, int l, int r) {
+    if(l > tr || r < tl) return segment_tree_node();
+    if(l <= tl && r >= tr) return tree[node];
+    int tm = (tl + tr)>>1, left_node = node<<1, right_node = node<<1|1;
+    segment_tree_node resl, resr;
+    resl = queryST(left_node, tl, tm, l, r);
+    resr = queryST(right_node, tm + 1, tr, l, r);
+    return combine(resl, resr);
+}
+
+void updateST(int node, int tl, int tr, int position, segment_tree_node value) {
+    if(tl == tr) {
+        tree[node] = value;
+        return;
+    }
+    int tm = (tl + tr)>>1, left_node = node<<1, right_node = node<<1|1;
+    if(position <= tm) updateST(left_node, tl, tm, position, value);
+    else updateST(right_node, tm + 1, tr, position, value);
+    tree[node] = combine(tree[left_node], tree[right_node]);
 }
