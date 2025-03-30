217ccceec9a5c30851387a492cba0617fa8b3dd9
Saurabh Hota <saurabh060792@gmail.com>
2025-03-30 16:45:00 -0500

    Update pow.cpp

--- Algorithms/pow.cpp
+++ Algorithms/pow.cpp
@@ -11,9 +11,9 @@ typedef long long int lli;
 lli pow(lli a, lli exp) { 
     lli ans = 1;
     while(exp > 0) {
-        if(exp % 2 == 1) ans = (ans * a) % M2;
+        if(exp & 1) ans = (ans * a) % M2;
         a = (a * a) % M2;
-        exp /= 2;
+        exp >>= 1;
     }
    return ans;
 }
