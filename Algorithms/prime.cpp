4c24e16b89c695d12604028faea26c0633db9a82
Saurabh Hota <saurabh060792@gmail.com>
2025-03-30 16:56:19 -0500

    Update prime.cpp

--- Algorithms/prime.cpp
+++ Algorithms/prime.cpp
@@ -9,10 +9,16 @@
 using namespace std;
 
 typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef __int128 llli;
+typedef unsigned __int128 ullli;
 
 bool is_prime[MAX];
 vector<lli> primes;
 
+bool miller_rabin(ulli n);
+bool check_composite(ulli n, ulli a, ulli d, int s);
+ulli pow(ulli a, ulli exp, ulli mod);
 void sieve_of_eratosthenes();
 vector<pair<lli, lli>> prime_decomposition(lli x);
 
@@ -63,4 +69,46 @@ vector<pair<lli, lli>> prime_decomposition(lli x) {
             break;
         }
     } return answer;
+}
+
+ulli pow(ulli a, ulli exp, ulli mod) {
+    ulli result = 1;
+    a %= mod;
+    while (exp) {
+        if (exp & 1)
+            result = (ullli)result * a % mod;
+        a = (ullli)a * a % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+// For a base 2 <= a <= n-1 we check if either
+// a^d = 1 mod n OR a^(d*2^r) = -1 mod n where 0 <= r <= s -1 
+bool check_composite(ulli n, ulli a, ulli d, int s) {
+    ulli x = pow(a, d, n);
+    if (x == 1 || x == n - 1) return false;
+    for (int r = 1; r < s; r++) {
+        x = (ullli)x * x % n;
+        if (x == n - 1) return false;
+    }
+    return true;
+};
+
+// https://cp-algorithms.com/algebra/primality_tests.html
+bool miller_rabin(ulli n) {
+    if (n < 2) return false;
+
+    int r = 0;
+    ulli d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        r++;
+    }
+
+    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+        if (n == a) return true;
+        if (check_composite(n, a, d, r)) return false;
+    }
+    return true;
 }
