3f76de322a1e3adf4df63aca35f6c978b31fcee9
Saurabh Hota <saurabh060792@gmail.com>
2025-04-04 21:19:24 -0500

    Update STRCOUNT.cpp

--- Spoj/STRCOUNT.cpp
+++ Spoj/STRCOUNT.cpp
@@ -17,10 +17,19 @@ int main() {
         f[i][i] = 1;
     }
 
+    // f(n, k) = Last digit is zero + Last digit is one
+    //         = f(n-1, k) + 2nd last is zero + 2nd last is one
+    //         = f(n-1, k) + f(n-2, k) + 3rd last is zero + 3rd last is one
+    //         = so on till kth zero
+    // f(n, k) = {sum{i=1}{k} f(n-i, k)} + kth last is one
+    // If kth last is one then we already have a longest subarray of 1 of size k.
+    // So the k+1th last must be 0 otherwise we would have longest subarray of 1 of size k.
+    // Then rest of the (n-k-1) positions can have k or k-1 or k-2 ... or 0 size longest subarray of 1
+    // f(n, k) = {sum{i=1}{k} f(n-i, k)} + {sum{i=0}{k} f(n-k-1, i)}
     for (int n = 2; n < MAX; n++) {
         for (int k = 1; k < n; k++) {
-            for (int i = 1; i <= k; i++) f[n][k] += f[n - i][k];
-            for (int i = k; i >= 0; i--) f[n][k] += f[n - k - 1][i];
+            for (int i = 1; i <= k; i++) f[n][k] += f[n - i][k];  // {sum{i=1}{k} f(n-i, k)}
+            for (int i = k; i >= 0; i--) f[n][k] += f[n - k - 1][i];  // {sum{i=0}{k} f(n-k-1, i)}
         }
     }
 
