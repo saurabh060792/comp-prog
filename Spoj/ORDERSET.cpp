d2a537a454f4935ab878005d26e1eabff1d6ea3e
Saurabh Hota <saurabh060792@gmail.com>
2025-04-07 15:20:37 -0500

    Update ORDERSET.cpp

--- Spoj/ORDERSET.cpp
+++ Spoj/ORDERSET.cpp
@@ -10,6 +10,7 @@ using namespace std;
 typedef long long int lli;
 typedef tree<lli, null_type, less<lli>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
+// https://codeforces.com/blog/entry/11080
 int main() {
     int q;
     char q_type;
@@ -25,9 +26,11 @@ int main() {
         } else if (q_type == 'K') {
             if (num > t.size()) cout<<"invalid"<<endl;
             else {
+                // Kth smallest element.
                 auto it = t.find_by_order(num - 1);
                 cout<<*it<<endl;
             }
+        // Number of elements strictly smaller than num.
         } else cout<<t.order_of_key(num)<<endl;
     }
     return 0;
