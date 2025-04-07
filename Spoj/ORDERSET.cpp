0542a32d4762590da213c50ea85d28ae11eaa92e
Saurabh Hota <saurabh060792@gmail.com>
2025-04-07 14:45:04 -0500

    Create ORDERSET.cpp

---
+++ Spoj/ORDERSET.cpp
@@ -0,0 +1,34 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+
+#define MAX 100010
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int lli;
+typedef tree<lli, null_type, less<lli>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+int main() {
+    int q;
+    char q_type;
+    lli num;
+    ordered_set t;
+    cin>>q;
+    while(q--) {
+        cin>>q_type>>num;
+        if (q_type == 'I') {
+            t.insert(num);
+        } else if (q_type == 'D') {
+            t.erase(num);
+        } else if (q_type == 'K') {
+            if (num > t.size()) cout<<"invalid"<<endl;
+            else {
+                auto it = t.find_by_order(num - 1);
+                cout<<*it<<endl;
+            }
+        } else cout<<t.order_of_key(num)<<endl;
+    }
+    return 0;
+}
