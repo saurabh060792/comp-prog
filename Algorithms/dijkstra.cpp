13329499da24f072b4556720a8b7e36c543c2523
Saurabh Hota <saurabh060792@gmail.com>
2025-05-01 00:53:14 -0500

    Update dijkstra.cpp

--- Algorithms/dijkstra.cpp
+++ Algorithms/dijkstra.cpp
@@ -1,96 +1,75 @@
 #include <iostream>
-#include <cstdlib>
-#include <utility>
 #include <queue>
 #include <vector>
+#include <climits>
 
-#define MAX 1000
-#define INF 10000000
+#define MAX 200010
 
 using namespace std;
 
-typedef struct vertex
-{
-	int index,d,parent;														//parent in shortest path tree
-}vertex;
+typedef long long int lli;
+typedef pair<lli, lli> pll;
 
-struct compareGreater {
-	bool operator() (vertex v, vertex u)
-	{
-		return v.d > u.d;
-	}
+struct vertex {
+    lli d, parent;
 };
 
-vertex* dijkstra(vector<vector<int> > graph, int **weight, int s);
+void dijkstra(lli s);
 
-int main()
-{
-	int n,e,u,v,i,**weight,s;
-	vertex *V;
-	cin>>n>>e;
-	weight = (int**)malloc(n*sizeof(int*));
-	for(i=0;i<n;i++) weight[i] = (int*)malloc(n*sizeof(int));
-	vector<vector<int> > graph(n);
-	for(i=0;i<e;i++) {														//assuming the vertices are number from 0 to n-1
-		cin>>u>>v;
-		cin>>weight[u][v];
-		weight[v][u] = weight[u][v];
-		graph[u].push_back(v);
-		graph[v].push_back(u);
-	}
-	s = 0;
-	V = dijkstra(graph, weight, s);
-	for(i=0;i<n;i++) cout<<V[i].d<<endl;
-	return 0;
+vertex V[MAX];
+vector<pair<int, int>> graph[MAX];
+lli n, m;
+
+int main() {
+    lli u, v, w;
+    scanf("%lld%lld", &n, &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%lld%lld%lld", &u, &v, &w);
+        graph[u].push_back({v, w});
+    }
+    dijkstra(1);
+    for (int i = 1; i <= n; i++) printf("%lld ", V[i].d);
+    return 0;
 }
 
-vertex* dijkstra(vector<vector<int> > graph, int **weight, int s)
-{
-	int i,n = graph.size();
-	int visited[n];
-	vector<int>::iterator it;
-	vertex t,temp,*V = (vertex*)malloc(n*sizeof(vertex)),u;
-	priority_queue<vertex, vector<vertex>, compareGreater> Q;
-	t.d = 0;
-	t.index = s;
-	t.parent = -1;
-	Q.push(t);
-	for(i=0;i<n;i++) {
-		temp.d = INF;
-		temp.index = i;
-		Q.push(temp);
-	}
-	for(i=0;i<n;i++) visited[i] = 0;
-	while(!Q.empty()) {
-		u = Q.top();
-		Q.pop();
-		if(!visited[u.index]) {
-			V[u.index].index = u.index;
-			V[u.index].d = u.d;
-			visited[u.index] = 1;
-			for(it = graph[u.index].begin(); it != graph[u.index].end(); it++) {
-				temp.index = *it;
-				temp.d = u.d + weight[u.index][*it];
-				temp.parent = u.index;
-				Q.push(temp);
-			}
-		}
-	}
-
-	return V;
+void dijkstra(lli s) {
+    lli u, v, d;
+    // {distance, index} pair.
+    priority_queue<pll, vector<pll>, greater<pll>> Q;
+    for (int i = 1; i <= n; i++) V[i].d = LLONG_MAX;
+    V[s] = {0, 0};
+    Q.push({0, s});
+    while (!Q.empty()) {
+        u = Q.top().second;
+        d = Q.top().first;
+        Q.pop();
+	// A vertex can appear multiple times with different distance
+	// in the queue at the same time. So make sure the distance
+	// we get equals the distance stored in V[u].d (which is optimal)
+	// otherwise ignore and continue.
+	// More Details: https://usaco.guide/gold/shortest-paths?lang=cpp#implementation-1
+	//       __ (2)__       __(n+2)
+	//      /__ (3)__\     /__(n+3)
+	//     /     .     \  /     .
+	//  (1)      .    (n+1)     .
+	//     \     .      /  \    .
+	//      \    .     /    \   .
+	//       \__(n)___/      \__(2n)
+	// In the above example, edge weight can be such that (n+1) node is pushed to the 
+	// queue n-1 times, for each vertex (2) to (n). When we pop (n+1) vertex for the first
+	// time and relax its neighbors, we dont want to do it again n-2 times becuase then
+	// the time complexity would be n^2(log n) because (n+1) node is processed n-1 times
+	// and each time we will be going ove its 2n-1 neighbours.
+	// The if condition below prevent this from happening. That is for all later n-2 times when
+	// vertex (n+1) is popped it wont have the same distance as stored in V[n+1].d because
+	// V[n+1].d will contain the lowest distance which it got the first time when it is popped.
+        if (d != V[u].d) continue;
+        for (auto v : graph[u]) {
+            if (d + v.second < V[v.first].d) {
+                V[v.first].parent = u;
+                Q.push({V[v.first].d = d + v.second, v.first});
+            }
+        }
+    }
+    return;
 }
-
-
-
-// Sample Input
-// first line contain number of vertices and edges and next n line contains
-// edges and their weights.
-// 5 8
-// 0 1 5
-// 0 2 5
-// 0 4 1
-// 1 4 2
-// 1 3 1
-// 2 4 2
-// 2 3 2
-// 4 3 4
