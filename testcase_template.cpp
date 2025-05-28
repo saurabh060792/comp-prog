08ec0cd54603a30a2a27512a9048856618fceafb
Saurabh Hota <saurabh060792@gmail.com>
2025-05-28 12:37:38 -0500

    Create testcase_template.cpp

---
+++ testcase_template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define MAX 200010
+
+using namespace std;
+
+typedef long long int lli;
+typedef pair<lli, lli> pll;
+
+int main() {
+    lli t;
+    scanf("%lld", &t);
+    while (t--) solve();
+    return 0;
+}
+
+void solve() {
+    
+}
