5f5006141444358e0faed40e47b5ab064644bff5
Saurabh Hota <saurabh060792@gmail.com>
2025-05-31 18:37:10 -0500

    Update DQUERY.cpp

--- Spoj/DQUERY.cpp
+++ Spoj/DQUERY.cpp
@@ -1,81 +1,66 @@
 #include <cstdio>
-#include <cstring>
 #include <algorithm>
-
-#define MAXQUERY 200000
-#define MAXVAL 1000006
-#define MAXN 30004
+#include <cmath>
 
 using namespace std;
 
-struct event
-{
-	int ind,start,end;
-	
-	event(){};
-	event(int ind, int start, int end) : ind(ind), start(start), end(end) {};
-	
-	bool operator < (event E)const
-	{
-		return end < E.end;
-	}
-}e[MAXQUERY];
+#define MAX 30004
+// n/sqrt(q) https://codeforces.com/blog/entry/61203?#comment-451304
+#define MAXSQRT 70
+#define MAXQ 200005
+#define MAXNUM 1000006
 
-int sum(int idx);
-void update(int idx, int val);
+struct query {
+    int l, r, bucket, id;
+};
 
-int n,a[MAXN],T[MAXN],last[MAXVAL],q,ans[MAXQUERY];
+int hash[MAXNUM], a[MAX], ans[MAXQ], distinct = 0;
+query Q[MAXQ];
 
-int main()
-{
-	int i, x, y, pos = 0, tot = 0;
-	scanf("%d",&n);
-	for(i=0;i<n;i++) scanf("%d",&a[i]);
-	
-	scanf("%d",&q);
-	for(i=0;i<q;i++) {
-		scanf("%d %d",&x,&y);
-		e[i] = event(i,x,y);
-	}
+void add(int idx);
+void remove(int idx);
+bool cmp(const query &a, const query &b);
 
-	sort(e,e+q);
-	
-	memset(last,-1,sizeof(last));
-	memset(T,0,sizeof(T));
-	
-	//We maintain a BIT which will correctly answer all the distinct numbers between 0..i
-	//only if the query is sorted by it end index.
-	//If the number on which we are iterating if already seen(can easily be done by a hash)
-	//then delete(update(last[a[i]], -1)) the last seen entry and add 1 in the new position
-	//(update(i, 1)) and answer all the query which are ending in i since as we move on to
-	//the larger index and if any number is previously seen then we can safely delete that
-	//becausse the previously seen number position's query are already been answered
-
-	for(i=0;i<n;++i) {
-		if(last[a[i]] != -1) update(last[a[i]],-1);
-		else tot++;
-		
-		update(i+1,1);
-		last[a[i]] = i+1;
-		
-		while(pos < q && e[pos].end == i+1) {
-			ans[e[pos].ind] = tot - sum(e[pos].start-1);
-			pos++;
-		}
-	}
-	
-	for(i=0;i<q;i++) printf("%d\n",ans[i]);	
-	return 0;
+int main() {
+    int n, q, i, l, r;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) scanf("%d", &a[i]);
+    scanf("%d", &q);
+    for (i = 0; i < q; i++) {
+        scanf("%d%d", &Q[i].l, &Q[i].r);
+        Q[i].l--, Q[i].r--;
+        Q[i].id = i;
+        Q[i].bucket = Q[i].l / MAXSQRT;
+    }
+    sort(Q, Q + q, cmp);
+    l = 0, r = -1;
+    for (i = 0; i < q; i++) {
+        while (r < Q[i].r) r++, add(r);
+        while (l > Q[i].l) l--, add(l);
+        while (l < Q[i].l) remove(l), l++;
+        while (r > Q[i].r) remove(r), r--;
+        ans[Q[i].id] = distinct;
+    }
+    for (i = 0; i < q; i++) printf("%d\n", ans[i]);
 }
 
-void update(int idx, int val)
-{
-	for(;idx<=n;idx += (idx & -idx)) T[idx] += val;
+void add(int idx) {
+    hash[a[idx]]++;
+    if (hash[a[idx]] == 1) distinct++;
 }
 
-int sum(int idx)
-{
-	int sum = 0;
-	for(;idx>0;idx -= (idx & -idx)) sum += T[idx];
-	return sum;
+void remove(int idx) {
+    hash[a[idx]]--;
+    if (hash[a[idx]] == 0) distinct--;
+}
+
+// In odd blocks sort the right index in ascending order and
+// in even blocks sort it in descending order. This will
+// minimize the movement of right pointer, as the normal
+// sorting will move the right pointer from the end back to
+// the beginning at the start of every block. With the improved
+// version this resetting is no more necessary.
+bool cmp(const query &p, const query &q) {
+    if (p.bucket != q.bucket) return p.bucket < q.bucket;
+    return (p.bucket & 1) ? (p.r < q.r) : (p.r > q.r);
 }
