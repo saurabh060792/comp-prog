f119dc8b95763e12b4f2df1da5689eb377cb3873
Saurabh Hota <saurabh060792@gmail.com>
2025-06-19 15:07:34 -0500

    Update kruskal.cpp

--- Algorithms/Graph/kruskal.cpp
+++ Algorithms/Graph/kruskal.cpp
@@ -7,9 +7,21 @@
 using namespace std;
 
 #define MAX 400005
+#define MAXLOG 20
 
 typedef long long int lli;
 typedef pair<lli, lli> pll;
+
+struct vertex {
+    int parent, depth;
+    lli up_weight;  // weight from this node to is parent.
+};
+
+struct edge {
+    lli w;
+    int u, v, id;
+    bool operator<(const struct edge& e) { return w < e.w; }
+};
 
 struct dsu {
     vector<int> parent, size;
@@ -38,38 +50,62 @@ struct dsu {
 };
 
 lli kruskal();
+void dfs(int u, int p);
 
-vector<pair<int, int>> mst[MAX];
-pair<lli, pair<int, int>> edges[MAX];
+vertex V[MAX];
+edge edges[MAX];
 bool present_in_mst[MAX];
 int n, m;
+vector<pair<int, lli>> mst[MAX];
 
 int main() {
-    int u, v;
-    lli w;
+    int u, v, l;
+    lli w, mst_w;
     scanf("%d%d", &n, &m);
     for (int i = 0; i < m ; i++) {
         scanf("%d%d%lld", &u, &v, &w);
-        edges[i] = {w, {u, v}};
+        edges[i] = {w, u, v, i};
     }
+
+    mst_w = kruskal();
+    dfs(1, 0);
+
     return 0;
 }
 
 lli kruskal() {
     lli w = 0;
-    int u, v;
+    int u, v, pu, pv, id;
     sort(edges, edges + m);
     dsu d(n + 10);
     for (int i = 0; i < m; i++) {
-        u = d.find_set(edges[i].second.first);
-        v = d.find_set(edges[i].second.second);
-        if (u != v) {
-            d.union_sets(u, v);
-            w += edges[i].first;
-            present_in_mst[i] = true;
-            mst[u].push_back({v, w});
-            mst[v].push_back({u, w});
+        u = edges[i].u;
+        v = edges[i].v;
+        pu = d.find_set(u);
+        pv = d.find_set(v);
+        if (pu != pv) {
+            d.union_sets(pu, pv);
+            w += edges[i].w;
+            id = edges[i].id;
+            present_in_mst[id] = true;
+            mst[u].push_back({v, edges[i].w});
+            mst[v].push_back({u, edges[i].w});
         }
     }
     return w;
+}
+
+void dfs(int u, int p) {
+    int v;
+    lli w;
+    for (auto i : mst[u]) {
+        v = i.first;
+        w = i.second;
+        if (v == p) continue;
+        V[v].depth = V[u].depth + 1;
+        V[v].parent = u;
+        V[v].up_weight = w;
+        dfs(v, u);
+    }
+    return;
 }
