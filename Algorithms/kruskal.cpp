833377e717329b148a9eb6b86e997ecc7b777102
Saurabh Hota <saurabh060792@gmail.com>
2025-06-19 13:52:11 -0500

    Update kruskal.cpp

--- Algorithms/kruskal.cpp
+++ Algorithms/kruskal.cpp
@@ -1,82 +1,72 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
 #include <algorithm>
-#include <cstring>
-
-#define MAX 100005
-#define INF 1000000000
+#include <climits>
 
 using namespace std;
 
-typedef long long int lli;
+#define MAX 400005
 
-pair<lli, pair<lli, lli> > edge[MAX];
-lli parent[MAX],rank[MAX],n,m;
+typedef long long int lli;
+typedef pair<lli, lli> pll;
+
+struct dsu {
+    vector<int> parent, size;
+
+    dsu(int n) {
+        parent.resize(n);
+        for (int i = 0; i < n; i++) parent[i] = i;
+        size.resize(n, 0);
+    }
+
+    void union_sets(int u, int v) {
+        int pu, pv;
+        pu = find_set(u);
+        pv = find_set(v);
+        if (pu == pv) return;
+        if (size[pu] < size[pv]) swap(pu, pv);
+        parent[pv] = pu;
+        size[pu] += size[pv];
+        return;
+    }
+
+    int find_set(int v) {
+        if (v == parent[v]) return v;
+        return parent[v] = find_set(parent[v]);
+    }
+};
 
 lli kruskal();
-void createSet(lli x);
-lli find(lli x);
-lli merge(lli x, lli y);
 
-int main()
-{
-	lli i;
-	scanf("%lld%lld",&n,&m);
-	for(i=0;i<m;i++){
-		scanf("%lld%lld%lld",&edge[i].second.first,&edge[i].second.second,&edge[i].first);
-		createSet(edge[i].second.first);
-		createSet(edge[i].second.second);
-	}
-	sort(edge, edge+m);
-	printf("%lld\n",kruskal());
-	return 0;
+pair<lli, pair<int, int>> edges[MAX];
+bool present_in_mst[MAX];
+int n, m;
+
+int main() {
+    int u, v;
+    lli w;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m ; i++) {
+        scanf("%d%d%lld", &u, &v, &w);
+        edges[i] = {w, {u, v}};
+    }
+    return 0;
 }
 
-lli kruskal()
-{
-	lli i,u,v,w=0;
-	for(i=0;i<m;i++) {
-		u = find(edge[i].second.first);
-		v = find(edge[i].second.second);
-		if(u != v) {
-			merge(u,v);
-			w += edge[i].first;
-		}
-	}
-	return w;
-}
-
-void createSet(lli x)
-{
-	parent[x] = x;
-	rank[x] = 1;
-	return;
-}
-
-lli find(lli x)
-{
-	if(parent[x] == x) return x;
-	else return (parent[x] = find(parent[x]));
-}
-
-lli merge(lli x, lli y)
-{
-	lli xRank, yRank, xRep, yRep;
-	xRep = find(x);
-	yRep = find(y);
-	xRank = rank[xRep];
-	yRank = rank[yRep];
-
-
-	if(xRep == yRep) return xRep;
-	else if(xRank < yRank) {
-		return (parent[xRep] = yRep);
-	}
-	else if(xRank > yRank) {
-		return (parent[yRep] = xRep);
-	}
-	else {
-		parent[yRep] = xRep;
-		rank[xRep]++;
-		return xRep;
-	}
+lli kruskal() {
+    lli w = 0;
+    int u, v;
+    sort(edges, edges + m);
+    dsu d(n + 10);
+    for (int i = 0; i < m; i++) {
+        u = d.find_set(edges[i].second.first);
+        v = d.find_set(edges[i].second.second);
+        if (u != v) {
+            d.union_sets(u, v);
+            w += edges[i].first;
+            present_in_mst[i] = true;
+        }
+    }
+    return w;
 }
