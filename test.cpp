9a632629d981743250152174a98a5b2701adb420
Saurabh Hota <saurabh060792@gmail.com>
2025-06-30 23:03:24 +0000

    suffix array itmo cf

--- test.cpp
+++ test.cpp
@@ -1,75 +1,89 @@
-#include <bits/stdc++.h>
-
-#define MAX 1510
-#define PRIME_BASE 31
-#define HASH_MOD 9223372036854775783
-#define ALPHABET_SIZE 26
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 typedef long long int lli;
+typedef pair<lli, lli> pll;
 
-void compute_prime_powers();
-void compute_hash(string const& s);
+int const ALPHABET_SIZE = 256;
 
-__int128 prime_power[MAX], hash_value[MAX];
-int bad_character[ALPHABET_SIZE], num_bad_character[MAX][MAX];
-unordered_set<lli> unique_hash;
+vector<int> suffix_array(string s);
+vector<int> sort_cyclic_shifts(string s);
+bool substring_search(string t, string s, vector<int> p);
+bool less(string t, string s, int id);
 
 int main() {
+    int q;
     string s, t;
-    int n, k, num_good_substring = 0;
-
-    printf("here\n");
-
-    scanf("%s %s", s, t);
-
-    printf("here\n");
-
-    for (int i = 0; i < ALPHABET_SIZE; i++) bad_character[i] = (t[i] == 0);
-    scanf("%d", &k);
-    compute_prime_powers();
-
-    printf("here\n");
-    
-    compute_hash(s);
-
-    printf("here\n");
-
-    n = s.length();
-    for (int l = 1; l <= n; l++) {
-        for (int i = 1; i <= n - l + 1; i++) {
-            __int128 cur_h = (hash_value[i + l] - hash_value[i - 1] + HASH_MOD) % HASH_MOD;
-            cur_h = (cur_h * prime_power[n-i-1]) % HASH_MOD;
-            unique_hash.insert(cur_h);
-        }
+    cin >> t;
+    vector<int> p = suffix_array(t);
+    scanf("%d", &q);
+    while (q--) {
+        cin>>s;
+        cout<<((substring_search(t, s, p)) ? "Yes" : "No")<<"\n";
     }
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
-            if (i == j) num_bad_character[i][j] = bad_character[s[j]];
-            else num_bad_character[i][j] = num_bad_character[i][j-1] + bad_character[s[j]];
-        }
-    }
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) 
-            num_good_substring += (num_bad_character[i][j] <= k);
-    }
-    printf("%d\n", num_good_substring);
-
     return 0;
 }
 
-void compute_prime_powers() {
-    prime_power[0] = 1;
-    for (int i = 1; i < MAX; i++) prime_power[i] = (prime_power[i-1] * PRIME_BASE) % HASH_MOD;
+bool substring_search(string t, string s, vector<int> p) {
+    // for (auto i : p) printf("%d ", i);
+    // printf("\n");
+    int l = 0, r = p.size() - 1, m;
+    while (r - l > 1) {
+        m = (l + r)/2;
+        string temp = t.substr(p[m], s.size());
+        // cout<<temp<<endl;
+        if (temp < s) l = m;
+        else r = m;
+        // printf("l: %d r: %d\n", l, r);
+    }
+    return t.substr(p[r], s.size()) == s;
 }
 
-void compute_hash(string const& s) {
-    __int128 temp_hash = 0;
-    for (int i = 1; i <= s.length(); i++) {
-        temp_hash = (temp_hash + (s[i] - 'a' + 1) * prime_power[i]) % HASH_MOD;
-        hash_value[i] = temp_hash;
+vector<int> sort_cyclic_shifts(string s) {
+    int n = s.size();
+    vector<int> p(n), c(n), cnt(max(ALPHABET_SIZE, n), 0);
+    for (int i = 0; i < n; i++) cnt[s[i]]++;
+    for (int i = 1; i < ALPHABET_SIZE; i++) cnt[i] += cnt[i - 1];
+    for (int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
+    c[p[0]] = 0;
+    int classes = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[p[i]] != s[p[i - 1]])
+            classes++;
+        c[p[i]] = classes - 1;
     }
+
+    vector<int> pn(n), cn(n);
+    for (int k = 0; (1 << k) < n; k++) {
+        for (int i = 0; i < n; i++) {
+            pn[i] = p[i] - (1 << k);
+            if (pn[i] < 0) pn[i] += n;
+        }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (int i = 0; i < n; i++) cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++) cnt[i] += cnt[i - 1];
+        for (int i = n - 1; i >= 0; i--) p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << k)) % n]};
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << k)) % n]};
+            if (cur != prev) classes++;
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+        if (classes == n) break;
+    }
+    return p;
+}
+
+vector<int> suffix_array(string s) {
+    s += "$";
+    vector<int> sorted_shifts = sort_cyclic_shifts(s);
+    sorted_shifts.erase(sorted_shifts.begin());
+    return sorted_shifts;
 }
